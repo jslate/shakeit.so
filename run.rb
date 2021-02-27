b42af61f685bab0f0ded08ba298fb303019eea4d
Jonathan Slate <jslate@epionhealth.com>
2021-02-27 15:44:29 -0500

    duh

--- run.rb
+++ run.rb
@@ -43,6 +43,41 @@ get "/" do
   haml :home
 end
 
+get "/spot_auth" do
+  base = "https://accounts.spotify.com/authorize"
+  request_params = {
+    client_id: "be9bf6d4e3ab47b89f62ec63da06d3af",
+    response_type: "code",
+    redirect_uri: "https://shakeit-so-8a5sw.ondigitalocean.app/spot_auth_callback",
+    scope: "user-read-currently-playing user-read-playback-position",
+  }
+  url = "#{base}?#{request_params.map { |key, value| [key,value].join('=') }.join('&') }"
+  redirect url
+end
+
+get '/spot_auth_callback' do
+  code = params['code']
+  uri = URI("https://accounts.spotify.com/api/token")
+
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.set_form_data({
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: "https://shakeit-so-8a5sw.ondigitalocean.app/spot_auth_callback",
+    client_id: ENV["SPOTIFY_CLIENT_ID"],
+    client_secret: ENV["SPOTIFY_CLIENT_SECRET"],
+  })
+
+  response = http.request(request)
+  response.to_json
+
+  # request = Net::HTTP::Post.new(uri)
+  # request["Accept"] = "application/json"
+  # request["Content-Type"] = "application/json"
+  # request["Authorization"] = "Bearer #{ENV['SPOTIFY_TOKEN']}"
+  # response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(request) }
+end
+
 get '/player' do
   load_data
   haml :index, locals: data
