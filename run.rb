bec9536ee6ac73f46492a0f404829f60485ccf7d
Jonathan Slate <jslate@epionhealth.com>
2021-02-27 14:44:43 -0500

    disable google sheets for now

--- run.rb
+++ run.rb
@@ -4,14 +4,14 @@ require "sinatra"
 require "sinatra/json"
 require "pry"
 require "haml"
-require "./read_spreadsheet"
+# require "./read_spreadsheet"
 
 def now_playing
   uri = URI("https://api.spotify.com/v1/me/player/currently-playing")
   request = Net::HTTP::Get.new(uri)
   request["Accept"] = "application/json"
   request["Content-Type"] = "application/json"
-  request["Authorization"] = "Bearer #{File.read("./token").chomp}"
+  request["Authorization"] = "Bearer #{ENV['SPOTIFY_TOKEN']}"
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(request) }
   JSON.parse(response.body, object_class: OpenStruct)
 end
@@ -30,7 +30,7 @@ def data
     time_remaining: "-#{format_time(@now_playing.item.duration_ms.to_i - @now_playing.progress_ms.to_i)}",
     progress_width: "#{(@now_playing.progress_ms.to_f/@now_playing.item.duration_ms.to_f*100).floor}%",
     image: @now_playing.item.album.images[1].url,
-    notes: SpreadsheetReader.new.get_notes,
+    notes: [] # SpreadsheetReader.new.get_notes,
   }
 end
 
