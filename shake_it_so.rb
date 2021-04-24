99004e6f2b99aecfe8d28b54de0d88153e81e3e2
Jonathan Slate <jslate@epionhealth.com>
2021-04-24 11:21:55 -0400

    add cache control

--- shake_it_so.rb
+++ shake_it_so.rb
@@ -55,13 +55,13 @@ class ShakeItSo < Sinatra::Base
     @now_playing = SpotifyClient.new.now_playing
   end
 
-before do
-  expires 600, :public, :must_revalidate
-end
+  before do
+    expires 600, :public, :must_revalidate
+  end
 
-get '/' do
-  haml :index
-end
+  get '/' do
+    haml :index
+  end
 
   get '/about' do
     haml :about
