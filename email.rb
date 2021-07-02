1427ce346810361d2c32da2b7d9eaf628d0b0e68
Jonathan Slate <jslate@epionhealth.com>
2021-07-01 20:13:30 -0400

    update international page

--- email.rb
+++ email.rb
@@ -5,8 +5,8 @@ include SendGrid
 emails = JSON.parse(File.read("./emails.json")).map { |hash| hash.transform_keys(&:to_sym) }
 
 from = Email.new(email: 'dance@shakeit.so', name: "Shake It So")
-subject = 'Today! International Dance Celebration for Treehouse Foundation 🕺💃'
-content = Content.new(type: 'text/html', value: File.read("./public/email-drafts/june-5-2021.html"))
+subject = 'Hey dancers! 🕺💃'
+content = Content.new(type: 'text/html', value: File.read("./public/email-drafts/june-20-2021.html"))
 api_key = ENV['SENDGRID_API_KEY']
 sg = SendGrid::API.new(api_key: api_key)
 
