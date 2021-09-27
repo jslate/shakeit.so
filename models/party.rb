877918cb7f817b444364ac052614c04b2f4a7c0f
Jonathan Slate <jonathan.slate@pmglearing.com>
2021-09-27 18:45:04 -0400

    morning removed

--- models/party.rb
+++ models/party.rb
@@ -1,4 +1,13 @@
 require "sequel"
 
+DB = Sequel.connect(ENV.fetch("DATABASE_URL"))
+unless DB.tables.include?(:parties)
+  puts "create party table"
+  DB.create_table :parties do
+    primary_key :id
+    column :name, String
+  end
+end
+
 class Party < Sequel::Model(:parties)
 end
--- models/response.rb
+++ models/response.rb
@@ -1,4 +1,15 @@
 require "sequel"
 
+DB = Sequel.connect(ENV.fetch("DATABASE_URL"))
+unless DB.tables.include?(:responses)
+  puts "create response table"
+  DB.create_table :responses do
+    primary_key :id
+    column :name, String
+    column :note, String
+    column :created_at, DateTime
+  end
+end
+
 class Response < Sequel::Model(:responses)
 end
